706f15df365439822a0e491f1eb84040fd9b8564
Jeevan H V <JVMT1277@MGTECHSOFT.IN>
2025-12-19 15:09:04 +0530

    Initial demo-platform shared library

---
+++ src/test/java/com/demo/platfrom/PlatfromApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.platfrom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatfromApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
