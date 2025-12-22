f0f535d4db0b7b744df5822cb0583ea82736d017
Jeevan H V <JVMT1277@MGTECHSOFT.IN>
2025-12-22 16:08:31 +0530

    Add build.yml for CI/CD and update demo-platform

---
+++ src/main/java/com/demo/platfrom/exception/ApiException.java
@@ -0,0 +1,8 @@
+package com.demo.platfrom.exception;
+
+public class ApiException extends RuntimeException {
+
+    public ApiException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/demo/platfrom/util/Constants.java
@@ -0,0 +1,9 @@
+package com.demo.platfrom.util;
+
+public final class Constants {
+
+    private Constants() {
+    }
+
+    public static final String DEFAULT_SUCCESS_MESSAGE = "Request processed successfully";
+}
