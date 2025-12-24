767734fe9e42a599169ec92ce6828b5eb3977bc9
Jeevan H V <JVMT1277@MGTECHSOFT.IN>
2025-12-24 10:00:15 +0530

    Added few dto class

---
+++ src/main/java/com/demo/platfrom/dto/UserResponse.java
@@ -0,0 +1,15 @@
+package com.demo.platfrom.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserResponse {
+
+    private Long id;
+    private String username;
+    private String email;
+}
